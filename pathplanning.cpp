546dc5cc82fb490018cb85815a72ae9ccf8e5749
Andrey Nedoluzhko <a.nedol@yandex.ru>
2017-11-17 15:21:53 +0300

    Add files via upload

---
+++ pathplanning.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "tinyxml.cpp"
+#include "tinyxml.h"
+#include "tinyxmlerror.cpp"
+#include "tinyxmlparser.cpp"
+#include "tinystr.cpp"
+#include "tinystr.h"
+
+using namespace std;
+
+int main() {
+    TiXmlDocument doc("/PathPlanning/pathplanning/example.xml");
+    if (!doc.LoadFile()) {
+        cout << "Load failed" << endl;
+        return 0;
+    }
+    TiXmlNode *it = doc.RootElement()->FirstChild();
+    while (it != NULL) {
+        cout << it->Value() << endl;
+        it = it->NextSibling();
+    }
+    doc.RootElement()->RemoveChild(doc.RootElement()->FirstChild());
+    doc.SaveFile("/PathPlanning/pathplanning/example_new.xml"); //"map" section is deleted
+    return 0;
+}
