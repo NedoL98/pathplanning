c1a5119fc9598e2ba1d307f8c23998f666841aed
Andrey Nedoluzhko <a.nedol@yandex.ru>
2017-12-05 21:20:31 +0300

    Working Djkstra added

--- isearch.cpp
+++ isearch.cpp
@@ -3,6 +3,8 @@
 #include <set>
 #include <time.h>
 
+const double EPS = 1e-7;
+
 ISearch::ISearch()
 {
     hweight = 1;
@@ -11,7 +13,122 @@ ISearch::ISearch()
 
 ISearch::~ISearch(void) {}
 
-bool canVisit(int x, int y, int dx, int dy, const Map &map, const EnvironmentOptions &options)
+double l_diff(double dx, double dy);
+
+SearchResult ISearch::startSearch(ILogger *Logger, const Map &map, const EnvironmentOptions &options)
+{
+    clock_t time_start = clock();
+
+    //Lambda-function as a std::set comparator
+    auto compareByCell = [](Node v1, Node v2) {
+        if (v1.i != v2.i) {
+            return v1.i < v2.i;
+        }
+        return v1.j < v2.j;
+    };
+
+    auto compareByDistance = [&](Node v1, Node v2) {
+        if (v1.g != v2.g) {
+            return v1.g < v2.g;
+        }
+        return compareByCell(v1, v2);
+    };
+
+    std::multiset<Node, decltype(compareByDistance)> open(compareByDistance);
+    std::multiset<Node, decltype(compareByDistance)> closed(compareByDistance);
+
+    std::set<Node, decltype(compareByCell)> distance(compareByCell); //Only visited nodes are stored here
+
+    Node goalNode = Node(map.getGoalPoint().first, map.getGoalPoint().second);
+    Node startingNode = Node(map.getStartingPoint().first, map.getStartingPoint().second);
+
+    open.insert(startingNode);
+    distance.insert(startingNode);
+
+    int step_counter = 0;
+
+    // std::cout << "Here\n" << std::endl;
+
+    while (!open.empty())
+    {
+        ++step_counter;
+        Node curNode = *open.begin(); //Get the nearest node
+        /*
+        std::cout << "Current node: " << std::endl;
+        std::cout << curNode.i << " " << curNode.j << std::endl;
+        std::cout << "Current distance: " << std::endl;
+        std::cout << curNode.g << std::endl;
+        */
+        open.erase(open.begin()); //Erase it from queue
+        closed.insert(curNode); //Mark it as a visited vertex
+
+        if (curNode.i == map.getGoalPoint().first and
+            curNode.j == map.getGoalPoint().second) //Goalpoint reached
+        {
+            break;
+        }
+
+        auto successors = findSuccessors(curNode, map, options);
+
+        for (auto nextNode : successors) { //Inserting new nodes
+            //If it's not visited yet or if new distance is better
+            auto it = distance.find(nextNode);
+            if (it == distance.end() or it->g > nextNode.g) {
+                if (it != distance.end()) {
+                    open.erase(*it); //We should delete this, because distance may be updated
+                }
+                open.insert(nextNode);
+                distance.insert(nextNode); //No need to delete this, because compared by cell only, g- doesn't matter
+            }
+        }
+    }
+
+    std::cout << "Search finished!" << std::endl;
+    //Restoring the path
+    auto curNode_ptr = closed.begin();
+    while (curNode_ptr != closed.end() and *curNode_ptr != goalNode) {
+        ++curNode_ptr;
+    }
+    if (curNode_ptr != closed.end()) {
+        lppath.push_front(*curNode_ptr);
+        while (*curNode_ptr != startingNode) {
+            --curNode_ptr;
+
+            // std::cout << curNode_ptr->i << " " << curNode_ptr->j << std::endl;
+            /*
+            if (l_diff(lppath.back().i - curNode_ptr->i, lppath.back().j - curNode_ptr->j) <= 2) {
+                std::cout << curNode_ptr->g << std::endl;
+                std::cout << l_diff(lppath.back().i - curNode_ptr->i, lppath.back().j - curNode_ptr->j) << std::endl;
+                std::cout << lppath.back().g << std::endl;
+                std::cout << std::endl;
+            }
+            */
+            if (fabs(curNode_ptr->g + l_diff(lppath.back().i - curNode_ptr->i,
+                                             lppath.back().j - curNode_ptr->j) - lppath.back().g) < EPS) {
+                lppath.push_back(*curNode_ptr);
+            }
+        }
+    }
+
+
+    //No need to find lpath, because it's the same as hpath
+
+    // std::cout << hppath.size() << std::endl;
+
+    clock_t time_finish = clock();
+
+    // std::cout << distance[map.getGoalPoint().first][map.getGoalPoint().second] << std::endl;
+
+    sresult.pathfound = (curNode_ptr != closed.end());
+    sresult.nodescreated = closed.size() + open.size();
+    sresult.numberofsteps = step_counter;
+    sresult.time = static_cast<double>(time_finish) - static_cast<double>(time_start);
+    sresult.hppath = &hppath; //Here is a constant pointer
+    sresult.lppath = &lppath;
+    return sresult;
+}
+
+bool check(int x, int y, int dx, int dy, const Map &map, const EnvironmentOptions &options)
 {
     if (dx == 0 and dy == 0) //Zero movement case
     {
@@ -40,105 +157,22 @@ bool canVisit(int x, int y, int dx, int dy, const Map &map, const EnvironmentOpt
     return true;
 }
 
-SearchResult ISearch::startSearch(ILogger *Logger, const Map &map, const EnvironmentOptions &options)
+double l_diff(double dx, double dy) {
+    return sqrt(dx * dx + dy * dy);
+}
+
+std::list<Node> ISearch::findSuccessors(Node &curNode, const Map &map, const EnvironmentOptions &options)
 {
-    clock_t time_start = clock();
-
-    std::vector<std::vector<int>> distance(map.getMapHeight(), std::vector<int>(map.getMapWidth(), -1));
-    auto compareByDistance = [&](std::pair<int, int> v1, std::pair<int, int> v2) //Lambda-function as a std::set comparator
-    {
-        return distance[v1.first][v1.second] < distance[v2.first][v2.second];
-    };
-
-    // std::cout << map.getValue(map.getStartingPoint().first, map.getStartingPoint().second) << std::endl;
-    // std::cout << map.getValue(map.getGoalPoint().first, map.getGoalPoint().second) << std::endl;
-
-    std::vector<std::pair<int, int>> visited;
-    std::multiset<std::pair<int, int>, decltype(compareByDistance)> current_queue(compareByDistance);
-    std::map<std::pair<int, int>, std::pair<int, int>> ancestor;
-
-    distance[map.getStartingPoint().first][map.getStartingPoint().second] = 0;
-    current_queue.insert(map.getStartingPoint());
-
-    int step_counter = 0;
-
-    while (!current_queue.empty())
-    {
-        std::pair<int, int> v = *current_queue.begin(); //Get the nearest node
-        /*
-        std::cout << "Current node: " << std::endl;
-        std::cout << v.first << " " << v.second << std::endl;
-        std::cout << "Current distance: " << std::endl;
-        std::cout << distance[v.first][v.second] << std::endl;
-        */
-        current_queue.erase(current_queue.begin()); //Erase it from queue
-        visited.push_back(v); //Mark it as a visited vertex
-
-        if (v.first == map.getGoalPoint().first and
-            v.second == map.getGoalPoint().second) //Goalpoint reached
-        {
-            break;
-        }
-
-        for (int dx = -1; dx <= 1; ++dx) //Searching for a new node
-        {
-            for (int dy = -1; dy <= 1; ++dy)
-            {
-                //Checking whether it is possible to visit new vertex
-
-                if (canVisit(v.first, v.second, dx, dy, map, options))
-                {
-                    ++step_counter;
-                    //Checking whether we have to visit new vertex
-                    if (distance[v.first + dx][v.second + dy] == -1 or
-                        distance[v.first + dx][v.second + dy] > distance[v.first][v.second] + 1)
-                    {
-                        current_queue.erase({v.first + dx, v.second + dy});
-                        distance[v.first + dx][v.second + dy] = distance[v.first][v.second] + 1;
-                        current_queue.insert({v.first + dx, v.second + dy});
-
-                        ancestor[{v.first + dx, v.second + dy}] = v;
-                    }
-                }
+    std::list<Node> successors;
+    for (int dx = -1; dx <= 1; ++dx) {
+        for (int dy = -1; dy <= 1; ++dy) {
+            if (check(curNode.i, curNode.j, dx, dy, map, options)) {
+                successors.push_back(Node(curNode.i + dx, curNode.j + dy, curNode.g + l_diff(dx, dy)));
             }
         }
     }
-
-    if (distance[map.getGoalPoint().first][map.getGoalPoint().second] != -1) //Restoring the path
-    {
-        std::pair<int, int> current_vertex = {map.getGoalPoint().first, map.getGoalPoint().second};
-        while (current_vertex.first != map.getStartingPoint().first or
-               current_vertex.second != map.getStartingPoint().second)
-        {
-            lppath.push_front(Node(current_vertex.first, current_vertex.second));
-            current_vertex = ancestor[current_vertex];
-        }
-        lppath.push_front(Node(current_vertex.first, current_vertex.second));
-    }
-
-    //No need to find lpath, because it's the same as hpath
-
-    // std::cout << hppath.size() << std::endl;
-
-    clock_t time_finish = clock();
-
-    std::cout << distance[map.getGoalPoint().first][map.getGoalPoint().second] << std::endl;
-
-    sresult.pathfound = (distance[map.getGoalPoint().first][map.getGoalPoint().second] != -1);
-    sresult.nodescreated = visited.size();
-    sresult.numberofsteps = step_counter;
-    sresult.time = static_cast<double>(time_finish) - static_cast<double>(time_start);
-    sresult.hppath = &hppath; //Here is a constant pointer
-    sresult.lppath = &lppath;
-    return sresult;
-}
-
-/*std::list<Node> ISearch::findSuccessors(Node curNode, const Map &map, const EnvironmentOptions &options)
-{
-    std::list<Node> successors;
-    //need to implement
     return successors;
-}*/
+}
 
 /*void ISearch::makePrimaryPath(Node curNode)
 {
--- node.h
+++ node.h
@@ -16,16 +16,26 @@ struct Node
 {
     int     i, j; //grid cell coordinates
     double  F, g, H; //f-, g- and h-values of the search node
-    Node    *parent; //backpointer to the predecessor node (e.g. the node which g-value was used to set the g-velue of the current node)
+    Node    *parent; //backpointer to the predecessor node (e.g. the node which g-value was used to set the g-value of the current node)
+
+
+    Node (int x, int y, double dist = 0, Node *ancestor = nullptr)
+    {
+        i = x;
+        j = y;
+        g = dist;
+        parent = ancestor;
+    }
+
+    Node() = default;
 
     bool operator== (const Node &other) const {
         return i == other.i && j == other.j;
     }
 
-    Node (int x, int y)
-    {
-        i = x;
-        j = y;
+    bool operator != (const Node &other) const {
+        return !(*this == other);
     }
 };
+
 #endif
